735b5d2ba512bafb36c524d630721cbd0b42b5e5
enoch tamulonis <yungboiiindigo@gmail.com>
2024-02-07 13:27:38 -0600

    fix the generator

--- lib/generators/tailwind_devise/views_generator.rb
+++ lib/generators/tailwind_devise/views_generator.rb
@@ -5,10 +5,9 @@ module TailwindDevise
     source_root File.expand_path('templates', __dir__)
     desc "This generator adds the devise views styled with Tailwind CSS"
 
-    def install
+    def copy_devise_tailwind_files_to_app
       devise_folder_path = "#{Rails.root}/app/views/devise"
-      devise_folder = File.dirname(devise_folder_path)
-      FileUtils.rm_rf(devise_folder) if File.directory?(devise_folder)
+      FileUtils.rm_rf(devise_folder_path)
       FileUtils.mkdir_p(devise_folder_path)
       directory 'devise', devise_folder_path
     end
