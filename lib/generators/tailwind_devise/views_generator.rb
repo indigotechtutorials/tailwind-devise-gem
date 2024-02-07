cb25b3f1dd6a0939f569e8e9ab952e389f9fb102
enoch tamulonis <yungboiiindigo@gmail.com>
2024-02-07 13:19:54 -0600

    update views generator

--- lib/generators/tailwind_devise/views_generator.rb
+++ lib/generators/tailwind_devise/views_generator.rb
@@ -6,10 +6,11 @@ module TailwindDevise
     desc "This generator adds the devise views styled with Tailwind CSS"
 
     def install
-      devise_folder = File.dirname("#{Rails.root}/app/views/devise")
+      devise_folder_path = "#{Rails.root}/app/views/devise"
+      devise_folder = File.dirname(devise_folder_path)
       FileUtils.rm_rf(devise_folder) if File.directory?(devise_folder)
-      FileUtils.mkdir_p(devise_folder)
-      directory 'devise', devise_folder
+      FileUtils.mkdir_p(devise_folder_path)
+      directory 'devise', devise_folder_path
     end
   end
 end
