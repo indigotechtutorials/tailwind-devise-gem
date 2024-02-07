d13c450c874e6d085fdd68a29bf13da97fe996ca
enoch tamulonis <yungboiiindigo@gmail.com>
2024-02-07 13:13:06 -0600

    putting the templats in generators namespace

--- lib/generators/tailwind_devise/views_generator.rb
+++ lib/generators/tailwind_devise/views_generator.rb
@@ -5,7 +5,7 @@ module TailwindDevise
     source_root File.expand_path('templates', __dir__)
     desc "This generator adds the devise views styled with Tailwind CSS"
 
-    def copy_tailwind_devise_folders_into_app
+    def install
       devise_folder = File.dirname("#{Rails.root}/app/views/devise")
       FileUtils.rmdir(devise_folder) if File.directory?(devise_folder)
       FileUtils.mkdir_p(devise_folder)
